3a42944e784ede4e70163a3d08210cf46de745fb
Uriel2404 <155695320+Uriel2404@users.noreply.github.com>
2025-11-27 23:03:37 -0600

    Add Empleado model with schema definition

--- models/Empleado.js
+++ models/Empleado.js
@@ -1,1 +1,10 @@
+import mongoose from "mongoose";
 
+const EmpleadoSchema = new mongoose.Schema({
+  nombre: String,
+  puesto: String,
+  departamento: String,
+  correo: String,
+});
+
+export default mongoose.model("Empleado", EmpleadoSchema);
