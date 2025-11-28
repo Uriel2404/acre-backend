941451c7169587d30339c7fa44d6aae250261dbc
Uriel2404 <155695320+Uriel2404@users.noreply.github.com>
2025-11-27 23:02:08 -0600

    Add CRUD routes for empleados

--- routes/empleados.routes.js
+++ routes/empleados.routes.js
@@ -1,1 +1,14 @@
+import express from "express";
+import {
+  obtenerEmpleados,
+  crearEmpleado,
+  eliminarEmpleado
+} from "../controllers/empleados.controller.js";
 
+const router = express.Router();
+
+router.get("/", obtenerEmpleados);
+router.post("/", crearEmpleado);
+router.delete("/:id", eliminarEmpleado);
+
+export default router;
