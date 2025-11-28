bccb2d77442fc9e5049c9e4e839d4f336272b853
Uriel2404 <155695320+Uriel2404@users.noreply.github.com>
2025-11-27 23:03:23 -0600

    Add Documento schema for MongoDB model

--- models/Documento.js
+++ models/Documento.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+const DocumentoSchema = new mongoose.Schema({
+  nombre: String,
+  url: String,
+  categoria: String,
+  departamento: String,
+  fecha: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Documento", DocumentoSchema);
